4d581fa0c5acb5312971408eedbe79d7cedd1a9b
jongwon417 <jongwon417@gmail.com>
2022-04-01 10:03:53 +0900

    boilerweb

---
+++ Boiler/src/main/java/com/project/boiler/Boiler.java
@@ -0,0 +1,13 @@
+package com.project.boiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boiler {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boiler.class, args);
+	}
+
+}
---
+++ Boiler/src/main/java/com/project/boiler/Member.java
@@ -0,0 +1,26 @@
+package com.project.boiler;
+
+public class Member {
+	private String id;
+	private String on_off;
+	private int temp;
+	
+	public String getid() {
+		return id;
+	}
+	public void setid(String id) {
+		this.id = id;
+	}
+	public String getOn_off() {
+		return on_off;
+	}
+	public void setOn_off(String on_off) {
+		this.on_off = on_off;
+	}
+	public int getTemp() {
+		return temp;
+	}
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}	
+}
